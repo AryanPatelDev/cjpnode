3f0f90c03bc349a54031a681e40c2e81e9717e69
Aryan Sureshkumar Patel <offaryanpatel@gmail.com>
2025-04-21 11:31:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://aryanpateldev.github.io", "http://localhost:19006"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);app.use(express.json());
 
 // Simple health check endpoint
 app.get("/", (req, res) => res.send("CJ Parikh API running"));
